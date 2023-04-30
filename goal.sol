c5397abf2dbdabeeb9d90d27f8b2ca76aa701d18
Nils Martel <nilsmartel@yahoo.de>
2023-04-30 12:50:25 +0200

    Fix solar syntax

--- goal.sol
+++ goal.sol
@@ -1,8 +1,19 @@
 
+# for now
+# - no imports
+# - no modules
+# - no overloading
+# - no generics
+# - no Interfaces
+
+# but
+# - static typing
+# - function resolve
+# - buildin functions
 
 fun main() =
     let n = readline,
-    let g = greet n
+        g = greet n
     in
         println g
 
