a64dc36a55e33674233366408aaeaf3f58dcb009
Nils Martel <nilsmartel@yahoo.de>
2023-05-01 20:13:35 +0200

    Implement string methods for values

--- src/value.rs
+++ src/value.rs
@@ -1,4 +1,7 @@
-use std::rc::Rc;
+use std::{
+    fmt::{self, write},
+    rc::Rc,
+};
 
 pub type GcString = Rc<String>;
 
@@ -12,6 +15,18 @@ pub enum Value {
     String(GcString),
 }
 
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Void => write!(f, "void"),
+            Value::Bool(v) => write!(f, "{v}"),
+            Value::Int(i) => write!(f, "{i}"),
+            Value::Float(i) => write!(f, "{i}"),
+            Value::String(i) => write!(f, "{i}"),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum Int {
     Int64(i64),
@@ -22,6 +37,22 @@ pub enum Int {
     Uint32(u32),
     Uint16(u16),
     Uint8(u8),
+}
+
+impl fmt::Display for Int {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Int::*;
+        match self {
+            Int64(v) => write!(f, "{v}"),
+            Int32(v) => write!(f, "{v}"),
+            Int16(v) => write!(f, "{v}"),
+            Int8(v) => write!(f, "{v}"),
+            Uint64(v) => write!(f, "{v}"),
+            Uint32(v) => write!(f, "{v}"),
+            Uint16(v) => write!(f, "{v}"),
+            Uint8(v) => write!(f, "{v}"),
+        }
+    }
 }
 
 impl From<String> for Value {
