70833554e63bc9788937de0e418cb33b9f688479
Nils Martel <nilsmartel@yahoo.de>
2023-05-06 14:20:31 +0200

    Document some ideas

--- Documentation/Libraries-and-Modules.md
+++ Documentation/Libraries-and-Modules.md
@@ -23,3 +23,43 @@ e.g. "std(solar-lang)/0.0.1"
 .
 |- solar.yaml
 |- files.. .sol
+
+
+## Modules
+
+Structures for resolving Modules
+and Symbols inside modules:
+
+    ProjectConfig
+        # (only) for the current project (and local libs), an publisher will be ommitted
+        basepath: ["nice-gui()", "2.1.3"],
+        depmap:
+            "std" => ["std(solar-lang)", "0.0.1"]
+
+    ModuleConfig
+        project_id: usize
+        # include projectconfig reference?
+        # include basepath?
+        # path: ["types"]
+        files: [FileContext("string.sol"), FileContext("string_util.sol")]
+        # TODO static_functions: Array<{name: String, args: Vec<Type>, ret: Type}>
+
+    FileContext    (owned by ModuleConfig)
+        # notice, how the function names are not path of their path (e.g. module identifier)
+        imports:
+            # e.g.
+            # use @std.string.concat
+            "concat" => ["std(solar-lang)", "0.0.1", "string"]
+            # use util.(flipbits, redraw)
+            "flipbits" => ["nice-gui()", "0.0.1", "util" ]
+            "redraw" => ["nice-gui()", "0.0.1", "util"]
+        ast: Ast
+        # TODO compiled functions here?
+        # or on module level?
+        # ==> on module level, because +
+            1.) we need file distinction only for resolving imports
+            2.) we have a flat hierarchy inside a module.
+
+}
+
+*/
--- src/main.rs
+++ src/main.rs
@@ -28,3 +28,38 @@ fn main() {
     //    # deps are needed here, to know which VERSION the deps in this file are supposed to resolve to
     //    modules[module].append(FileContext::from_file(module, file=fullpath, deps=deps))
 }
+
+/*
+    ProjectConfig
+        # (only) for the current project (and local libs), an publisher will be ommitted
+        basepath: ["nice-gui()", "2.1.3"],
+        depmap:
+            "std" => ["std(solar-lang)", "0.0.1"]
+
+    ModuleConfig
+        project_id: usize
+        # include projectconfig reference?
+        # include basepath?
+        # path: ["types"]
+        files: [FileContext("string.sol"), FileContext("string_util.sol")]
+        # TODO static_functions: Array<{name: String, args: Vec<Type>, ret: Type}>
+
+    FileContext    (owned by ModuleConfig)
+        # notice, how the function names are not path of their path (e.g. module identifier)
+        imports:
+            # e.g.
+            # use @std.string.concat
+            "concat" => ["std(solar-lang)", "0.0.1", "string"]
+            # use util.(flipbits, redraw)
+            "flipbits" => ["nice-gui()", "0.0.1", "util" ]
+            "redraw" => ["nice-gui()", "0.0.1", "util"]
+        ast: Ast
+        # TODO compiled functions here?
+        # or on module level?
+        # ==> on module level, because +
+            1.) we need file distinction only for resolving imports
+            2.) we have a flat hierarchy inside a module.
+
+}
+
+*/
