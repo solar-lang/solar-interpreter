b9799dd5dcafdda9501caf080aa7d268f5fc10b8
Nils Martel <nilsmartel@yahoo.de>
2023-06-16 16:33:56 +0200

    Adjust TypeID

--- src/id.rs
+++ src/id.rs
@@ -3,5 +3,20 @@ use crate::util::IdPath;
 pub type SymbolId = (IdModule, IdFile, IdItem);
 
 pub type IdModule = IdPath;
-pub type IdFile = u32;
-pub type IdItem = u32;
+pub type IdFile = u16;
+
+#[derive(Debug, Copy, Clone)]
+pub enum IdItem {
+    // Variable
+    Global(u16),
+
+    /// Pointing to a function declared in the global scope
+    Func(u16),
+
+    /// Points to a Type declared in the global scope
+    Type(u16),
+
+    /// Describes a auto-derived Method in terms of referencing the type and specific field OR Enum and specific Variant
+    /// Note, enum variants MAY be constant
+    Method(u16, u16),
+}
