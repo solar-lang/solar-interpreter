f1e08d17031ce5904b24a938e6cb6160bdee97db
Nils Martel <nilsmartel@yahoo.de>
2023-10-31 20:21:57 +0100

    Comment code

--- solarpath/libraries/std(solar-lang)/0.0.1/types/int/int.sol
+++ solarpath/libraries/std(solar-lang)/0.0.1/types/int/int.sol
@@ -3,10 +3,14 @@ buildin_type Bool
 buildin_type Int8
 buildin_type Int16
 buildin_type Int32
+# the standard Int type has 64 Bits
 buildin_type Int
 buildin_type Uint8
 buildin_type Uint16
 buildin_type Uint32
+# the standard Uint type has 64 Bits
 buildin_type Uint
 buildin_type Float32
+
+# the standard Float type has 64 Bits
 buildin_type Float,
